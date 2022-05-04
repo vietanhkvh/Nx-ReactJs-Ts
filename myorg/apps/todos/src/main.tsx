23cadf4c6b48af83cb8d838ddfc1754c9cdeddea
vietanhkvh <58767285+vietanhkvh@users.noreply.github.com>
2022-05-04 14:27:25 +0700

    add storybook

--- myorg/apps/todos/src/main.tsx
+++ myorg/apps/todos/src/main.tsx
@@ -2,12 +2,28 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import { TODOS_FEATURE_KEY, todosReducer } from './app/todos.slice';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = configureStore({
+  reducer: { [TODOS_FEATURE_KEY]: todosReducer },
+  // Additional middleware can be passed to this array
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production',
+  // Optional Redux store enhancers
+  enhancers: [],
+});
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
